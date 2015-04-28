951a10f2fc2dc3ba68c9133f4cf213a666798621
Michaël Zasso <mic.besace@gmail.com>
2015-04-28 10:57:07 +0200

    main files and deps

---
+++ src/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var SyncDB = exports.SyncDB = require('sync-db');
+var IDBDriver = exports.IDBDriver = require('sync-db-idb');
+
+exports.sync = function (name, url, driver) {
+    if (typeof name !== 'string')
+        throw new TypeError('name argument must be a string');
+    if (driver === undefined)
+        driver = 'idb';
+    if (driver !== 'idb')
+        throw new RangeError('only "idb" is supported as a driver');
+    return new SyncDB({
+        driver: new IDBDriver(name),
+        url: url
+    });
+};
